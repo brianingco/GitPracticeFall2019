23c9045e5daacc9f19cd19797a4edce2191388d8
brianingco <brian.ingco@yahoo.com>
2020-02-29 13:52:11 -0700

    Made an update using the green check mark

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -8,6 +8,7 @@ public class LoopPractice {
         for (int i = 0; i < 10 ; i+=2) {
             System.out.println("Counter" + i);
             System.out.println("Adding another line for practice");
+            System.out.println("trying to commit using the green check mark");
 
         }
     }
